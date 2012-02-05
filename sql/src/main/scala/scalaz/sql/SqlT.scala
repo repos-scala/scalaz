abde4eedf2a28728c8c3a63144faa75b86f44a12
Tony Morris <tmorris@tmorris.net>
2012-02-05 10:29:23 +1000

    flatMap

--- sql/src/main/scala/scalaz/sql/SqlT.scala
+++ sql/src/main/scala/scalaz/sql/SqlT.scala
@@ -1,10 +1,28 @@
 package scalaz
 package sql
 
+// Note this is not a monad but does have flatMap
 sealed trait SqlT[F[_], A] {
   private[sql] val value: F[(Boolean, Trace, Either[SqlExceptionContext, A])] = this match {
     case SqlTImpl(x) => x
   }
+
+  def map[B](f: A => B)(implicit F: Functor[F]): SqlT[F, B] =
+    SqlTImpl(F.map(value) { case (p, t, z) => (p, t, z.right map f) })
+
+  def flatMap[B](f: A => SqlT[F, B])(implicit F: Monad[F]): SqlT[F, B] =
+    SqlTImpl(F.bind(value) {
+      case (p, t, y) => y match {
+        case Left(e) => F.point(p, t, Left(e))
+        case Right(a) =>
+          F.map(f(a).value) {
+            case (q, u, z) => {
+              val r = p && q
+              (r, if(r) t ++ u else u, z)
+            }
+          }
+      }
+    })
 }
 private case class SqlTImpl[F[_], A](x: F[(Boolean, Trace, Either[SqlExceptionContext, A])]) extends SqlT[F, A]
 
@@ -17,12 +35,12 @@ trait SqlTFunctions {
   import Lens._
   import CoStateT._
 
-  def traceSwitchL[F[_], A](implicit F: Monad[F]): SqlT[F, A] @-@ F[Boolean] =
+  def traceSwitchTL[F[_], A](implicit F: Monad[F]): SqlT[F, A] @-@ F[Boolean] =
     lens(s => coState((k => SqlTImpl(F.map2(s.value, k){
       case ((_, b, c), x) => (x, b, c)
     }), F.map(s.value) { case (a, _, _) => a })))
 
-  def traceL[F[_], A](implicit F: Monad[F]): SqlT[F, A] @-@ F[Trace] =
+  def traceTL[F[_], A](implicit F: Monad[F]): SqlT[F, A] @-@ F[Trace] =
     lens(s => coState((k => SqlTImpl(F.map2(s.value, k){
       case ((a, _, c), x) => (a, x, c)
     }), F.map(s.value) { case (_, b, _) => b })))
--- sql/src/main/scala/scalaz/sql/Trace.scala
+++ sql/src/main/scala/scalaz/sql/Trace.scala
@@ -4,13 +4,27 @@ package sql
 import Trace._
 
 trait Trace {
+  private[sql] val value: TrO = this match {
+    case TraceImpl(x) => x
+  }
 
+  def ++(t: Trace): Trace =
+    TraceImpl(value ++ t.value)
 }
-private case class TraceImpl(x: Tr[TraceOp]) extends Trace
+private case class TraceImpl(x: TrO) extends Trace
 
 object Trace extends TraceFunctions
 
 trait TraceFunctions {
   type Tr[A] =
   DList[A]
+
+  type TrO =
+    Tr[TraceOp]
+
+  implicit val TraceMonoid: Monoid[Trace] = new Monoid[Trace] {
+    val zero = TraceImpl(DList[TraceOp]())
+    def append(a: Trace, b: => Trace) = a ++ b
+ }
+
 }
